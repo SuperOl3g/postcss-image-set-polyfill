d4a7600cf297accc7cb0475db925d1ddf588300d
Romeo Van Snick <romeovs@gmail.com>
2016-02-06 12:30:38 +0100

    add tests for image path

---
+++ test/path.js
@@ -0,0 +1,58 @@
+var expect  = require('chai').expect;
+
+var postcss = require('postcss');
+var imageSet = require('../');
+
+var test = function (input, output, opts, done) {
+    expect(postcss(imageSet).process(input).css.replace(/[ \n]/g, '')).to.eql(output.replace(/[ \n]/g, ''));
+    done();
+};
+
+describe('postcss-image-set', function () {
+    it('parses the image-set w/o url', function (done) {
+        var input = `
+          a {
+            background-image: image-set(
+                "img/test.png" 1x,
+                "img/test-2x.png" 2x,
+                "my-img-print.png" 600dpi
+            );
+          }
+        `;
+
+        var output = `
+          a {
+              background-image: url("img/test.png");
+              background-image: image-set(
+                  "img/test.png" 1x,
+                  "img/test-2x.png" 2x,
+                  "my-img-print.png" 600dpi
+              );
+          }
+
+          @media (screen and min-resolution: 2dppx){
+              a{
+                  background-image: url("img/test-2x.png");
+                  background-image: image-set(
+                      "img/test.png" 1x,
+                      "img/test-2x.png" 2x,
+                      "my-img-print.png" 600dpi
+                  );
+              }
+          }
+
+          @media (screen and min-resolution: 600dpi){
+              a{
+                  background-image: url("my-img-print.png");
+                  background-image: image-set(
+                      "img/test.png" 1x,
+                      "img/test-2x.png" 2x,
+                      "my-img-print.png" 600dpi
+                  );
+              }
+          }
+      `;
+
+        test(input, output, {}, done);
+    });
+});
