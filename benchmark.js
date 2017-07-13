b423b69accf88869b960a34e78c5cf6cbb3bd9ca
Oleg Lykasov <oleglykasov@gmail.com>
2017-07-14 01:53:24 +0300

    Added: benchmark script

---
+++ benchmark.js
@@ -0,0 +1,24 @@
+const Benchmark = require('benchmark');
+const postcss = require('postcss');
+const imageSet = require('./');
+
+
+const suite = new Benchmark.Suite;
+
+const input =
+    'a{' +
+        'background-image: image-set(' +
+            'url(img/test.png) 1x, ' +
+            'url(img/test-2x.png) 2x, ' +
+            'url(my-img-print.png) 600dpi ' +
+        ');' +
+    '}';
+
+suite
+    .add('Process CSS', () => {
+        postcss(imageSet).process(input);
+    })
+    .on('cycle', event => {
+        console.log(String(event.target));
+    })
+    .run({ 'async': true });
